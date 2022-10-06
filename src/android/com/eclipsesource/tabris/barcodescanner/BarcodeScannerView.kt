8c839243ac0db9a90d187f55f957890d3cca51cd
Moritz Post <moritzpost@gmail.com>
2022-10-06 11:39:16 +0200

    Migrate Android implementation to MLKit barcode scanner library
    The change removes the deprecated Mobile Vision barcode scanning
    library [1] and replaces it with the modern MLKit barcode scanner
    library [2].
    
    Since the MLKit library harmonizes very well with the input images
    produced by the CameraX library [3], the change also replaces the old
    SurfaceView based camera view implementation with the modern CameraX
    library. As a result the barcode scanning feature will be more robust
    on a wider set of devices.
    
    [1] https://developers.google.com/vision/documentation/android/barcodes-overview
    [2] https://developers.google.com/ml-kit/vision/barcode-scanning/android
    [3] https://developer.android.com/training/camerax

--- src/android/com/eclipsesource/tabris/barcodescanner/BarcodeScannerView.kt
+++ src/android/com/eclipsesource/tabris/barcodescanner/BarcodeScannerView.kt
@@ -2,206 +2,155 @@ package com.eclipsesource.tabris.barcodescanner
 
 import android.Manifest.permission.CAMERA
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.content.res.Configuration
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.camera.view.PreviewView.ScaleType
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import com.eclipsesource.tabris.android.ActivityScope
-import com.eclipsesource.tabris.android.ActivityState
-import com.eclipsesource.tabris.android.ActivityState.PAUSE
-import com.eclipsesource.tabris.android.ActivityState.RESUME
-import com.eclipsesource.tabris.android.Events
-import com.eclipsesource.tabris.android.post
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.MultiProcessor
-import com.google.android.gms.vision.Tracker
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.eclipsesource.tabris.android.Events.RequestPermissionsResultListener
+import com.eclipsesource.tabris.android.RemoteObject
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 
 val BARCODE_NAMES = mapOf(
-    Barcode.CODE_128 to "code128",
-    Barcode.CODE_39 to "code39",
-    Barcode.CODE_93 to "code93",
-    Barcode.CODABAR to "codabar",
-    Barcode.DATA_MATRIX to "dataMatrix",
-    Barcode.EAN_13 to "ean13",
-    Barcode.EAN_8 to "ean8",
-    Barcode.ITF to "itf",
-    Barcode.QR_CODE to "qr",
-    Barcode.UPC_A to "upcA",
-    Barcode.UPC_E to "upcE",
-    Barcode.PDF417 to "pdf417",
-    Barcode.AZTEC to "aztec"
+    Barcode.FORMAT_CODE_128 to "code128",
+    Barcode.FORMAT_CODE_39 to "code39",
+    Barcode.FORMAT_CODE_93 to "code93",
+    Barcode.FORMAT_CODABAR to "codabar",
+    Barcode.FORMAT_DATA_MATRIX to "dataMatrix",
+    Barcode.FORMAT_EAN_13 to "ean13",
+    Barcode.FORMAT_EAN_8 to "ean8",
+    Barcode.FORMAT_ITF to "itf",
+    Barcode.FORMAT_QR_CODE to "qr",
+    Barcode.FORMAT_UPC_A to "upcA",
+    Barcode.FORMAT_UPC_E to "upcE",
+    Barcode.FORMAT_PDF417 to "pdf417",
+    Barcode.FORMAT_AZTEC to "aztec"
 )
 
-enum class ScaleMode(val id: String) {
+enum class ScaleMode(val id: String, val scaleType: ScaleType) {
 
-  FIT("fit"), FILL("fill");
+    FIT("fit", ScaleType.FIT_CENTER), FILL("fill", ScaleType.FILL_CENTER);
 
-  companion object {
-    fun find(id: String?) = values().find { it.id == id }
-  }
+    companion object {
+        fun find(id: String?) = values().find { it.id == id }
+    }
 }
 
-@SuppressLint("ViewConstructor", "MissingPermission")
-class BarcodeScannerView(private val scope: ActivityScope)
-  : ViewGroup(scope.activity), Events.ActivityStateListener {
+@SuppressLint("ViewConstructor")
+class BarcodeScannerView(private val scope: ActivityScope, private val selector: CameraSelector) :
+    FrameLayout(scope.activity) {
 
-  var camera = CameraSource.CAMERA_FACING_BACK
-  var scaleMode: ScaleMode = ScaleMode.FIT
-    set(value) {
-      field = value
-      requestLayout()
-    }
-
-  private val rcPermissionCamera = 20001
-  private val multiProcessor = MultiProcessor.Builder<Barcode>(MultiProcessor.Factory {
-    object : Tracker<Barcode>() {
-      override fun onNewItem(id: Int, barcode: Barcode) {
-        scope.post {
-          remoteObject(this@BarcodeScannerView)?.notify("detect",
-              mapOf("format" to BARCODE_NAMES[barcode.format], "data" to barcode.rawValue))
+    var scaleMode: ScaleMode = ScaleMode.FIT
+        set(value) {
+            field = value
+            previewView.scaleType = field.scaleType
         }
-      }
+    private val rcPermissionCamera = 20001
+    private var cameraController: LifecycleCameraController? = null
+    private val previewView = PreviewView(scope.activity).also {
+        it.scaleType = scaleMode.scaleType
+        this.addView(it, MATCH_PARENT, MATCH_PARENT)
     }
-  }).build()
-  private val cameraView: SurfaceView = SurfaceView(scope.activity)
-  private var cameraSource: CameraSource? = null
-  private var surfaceAvailable = false
-  private var startRequested = false
-  private var formats = Barcode.ALL_FORMATS
 
-  init {
-    addView(cameraView)
-    cameraView.holder.addCallback(object : SurfaceHolder.Callback {
-      override fun surfaceCreated(holder: SurfaceHolder) {
-        surfaceAvailable = true
-        startWhenReady()
-      }
+    private class BarcodeAnalyzer(
+        private val scanner: BarcodeScanner, val remoteObject: RemoteObject?
+    ) : ImageAnalysis.Analyzer {
 
-      override fun surfaceDestroyed(holder: SurfaceHolder) {
-        surfaceAvailable = false
-      }
-
-      override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-      }
-    })
-    addOnLayoutChangeListener { _, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-      if (left != oldLeft || top != oldTop || right != oldRight || bottom != oldBottom) {
-        if (startRequested) {
-          stop()
-          start(formats)
-        }
-      }
-    }
-  }
-
-  override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-    val layoutWidth = right - left
-    val layoutHeight = bottom - top
-    val layoutRatio = layoutWidth.toFloat() / layoutHeight.toFloat()
-    var childWidth = layoutWidth
-    var childHeight = layoutHeight
-    cameraSource?.previewSize?.let {
-      val frameRatio = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-        it.height.toFloat() / it.width.toFloat()
-      } else {
-        it.width.toFloat() / it.height.toFloat()
-      }
-      if (scaleMode == ScaleMode.FIT && frameRatio > layoutRatio
-          || scaleMode == ScaleMode.FILL && frameRatio < layoutRatio) {
-        childHeight = (childWidth * (1f / frameRatio)).toInt()
-      } else {
-        childWidth = (childHeight * frameRatio).toInt()
-      }
-    }
-    val xOffset = layoutWidth / 2 - childWidth / 2
-    val yOffset = layoutHeight / 2 - childHeight / 2
-    for (i in 0 until childCount) {
-      getChildAt(i).layout(xOffset, yOffset, childWidth + xOffset, childHeight + yOffset)
-    }
-  }
-
-  override fun activityStateChanged(activityState: ActivityState, intent: Intent?) {
-    when (activityState) {
-      RESUME -> startWhenReady()
-      PAUSE -> cameraSource?.stop()
-      else -> {
-        // nothing to do
-      }
-    }
-  }
-
-  fun start(formats: Int) {
-    withCameraPermission({
-      startCamera(formats)
-    }, {
-      scope.remoteObject(this)?.notify("error", "error", "Camera permission not granted")
-    })
-  }
-
-  private fun withCameraPermission(successCallback: () -> Unit, errorCallback: () -> Unit) {
-    if (checkSelfPermission(scope.activity, CAMERA) == PERMISSION_GRANTED) {
-      successCallback.invoke()
-    } else {
-      scope.events.addRequestPermissionResultListener(object : Events.RequestPermissionsResultListener {
-        override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-          if (requestCode == rcPermissionCamera) {
-            scope.events.removeRequestPermissionResultListener(this)
-            if (grantResults.elementAtOrNull(0) == PERMISSION_GRANTED) {
-              successCallback.invoke()
-            } else {
-              errorCallback.invoke()
+        @ExperimentalGetImage
+        override fun analyze(imageProxy: ImageProxy) {
+            imageProxy.image?.let { mediaImage ->
+                val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                val image = InputImage.fromMediaImage(mediaImage, rotationDegrees)
+                scanner.process(image)
+                    .addOnSuccessListener { barcodes ->
+                        handleSuccess(barcodes, remoteObject)
+                        imageProxy.close()
+                    }
+                    .addOnFailureListener {
+                        remoteObject?.notify("error", "error", it.message)
+                        imageProxy.close()
+                    }
             }
-          }
         }
 
-      })
-      ActivityCompat.requestPermissions(scope.activity, arrayOf(CAMERA), rcPermissionCamera)
+        fun handleSuccess(barcodes: List<Barcode>, remoteObject: RemoteObject?) {
+            for (barcode in barcodes) {
+                remoteObject?.notify(
+                    "detect", mapOf(
+                        "format" to BARCODE_NAMES[barcode.format],
+                        "data" to barcode.rawValue
+                    )
+                )
+            }
+        }
     }
-  }
 
-  private fun startCamera(formats: Int) {
-    this.formats = formats
-    val detector = BarcodeDetector.Builder(scope.activity).setBarcodeFormats(formats).build().apply {
-      setProcessor(multiProcessor)
+    fun start(formats: Int) {
+        withCameraPermission(
+            successCallback = { startCamera(formats) },
+            errorCallback = {
+                scope.remoteObject(this)
+                    ?.notify("error", "error", "Camera permission not granted")
+            }
+        )
     }
-    if (!detector.isOperational) {
-      scope.remoteObject(this)?.notify("error",
-          "error", "Barcode scanner dependencies not available. Is device storage available?")
-      return
-    }
-    cameraSource = CameraSource.Builder(scope.activity, detector)
-        .setRequestedPreviewSize(1024, 768)
-        .setFacing(camera)
-        .setAutoFocusEnabled(true)
-        .setRequestedFps(30.0f)
-        .build()
-    startRequested = true
-    startWhenReady()
-  }
 
-  private fun startWhenReady() {
-    if (surfaceAvailable && startRequested) {
-      try {
-        cameraSource?.start(cameraView.holder)
-        requestLayout()
-      } catch (exception: Exception) {
-        scope.remoteObject(this)?.notify("error", "error", exception.message)
-      }
-    }
-  }
+    private fun withCameraPermission(successCallback: () -> Unit, errorCallback: () -> Unit) {
+        if (checkSelfPermission(scope.activity, CAMERA) == PERMISSION_GRANTED) {
+            successCallback.invoke()
+        } else {
+            scope.events.addRequestPermissionResultListener(object :
+                RequestPermissionsResultListener {
+                override fun onRequestPermissionsResult(
+                    requestCode: Int,
+                    permissions: Array<String>,
+                    grantResults: IntArray
+                ) {
+                    if (requestCode == rcPermissionCamera) {
+                        scope.events.removeRequestPermissionResultListener(this)
+                        if (grantResults.elementAtOrNull(0) == PERMISSION_GRANTED) {
+                            successCallback()
+                        } else {
+                            errorCallback()
+                        }
+                    }
+                }
 
-  fun stop() {
-    cameraSource?.stop()
-    cameraSource?.release()
-    startRequested = false
-    cameraSource = null
-    formats = Barcode.ALL_FORMATS
-  }
+            })
+            ActivityCompat.requestPermissions(scope.activity, arrayOf(CAMERA), rcPermissionCamera)
+        }
+    }
+
+    private fun startCamera(formats: Int) {
+        val scanner = BarcodeScanning.getClient(
+            BarcodeScannerOptions.Builder().setBarcodeFormats(formats).build()
+        )
+        val remoteObject = scope.remoteObject(this@BarcodeScannerView)
+        val barcodeAnalyzer = BarcodeAnalyzer(scanner, remoteObject)
+        val executor = ContextCompat.getMainExecutor(scope.activity)
+        cameraController = LifecycleCameraController(scope.activity).apply {
+            cameraSelector = selector
+            setImageAnalysisAnalyzer(executor, barcodeAnalyzer)
+            bindToLifecycle(scope.activity)
+        }
+        previewView.controller = cameraController
+    }
+
+    fun stop() {
+        cameraController?.unbind()
+    }
 
 }
--- src/android/com/eclipsesource/tabris/barcodescanner/BarcodeScannerViewHandler.kt
+++ src/android/com/eclipsesource/tabris/barcodescanner/BarcodeScannerViewHandler.kt
@@ -1,5 +1,7 @@
 package com.eclipsesource.tabris.barcodescanner
 
+import androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
+import androidx.camera.core.CameraSelector.DEFAULT_FRONT_CAMERA
 import com.eclipsesource.tabris.android.ActivityScope
 import com.eclipsesource.tabris.android.Property
 import com.eclipsesource.tabris.android.StringProperty
@@ -8,47 +10,44 @@ import com.eclipsesource.tabris.android.internal.ktx.getArrayOrNull
 import com.eclipsesource.tabris.android.internal.ktx.getStringOrNull
 import com.eclipsesource.tabris.android.internal.nativeobject.view.ViewHandler
 import com.eclipsesource.v8.V8Object
-import com.google.android.gms.vision.CameraSource.CAMERA_FACING_BACK
-import com.google.android.gms.vision.CameraSource.CAMERA_FACING_FRONT
-import com.google.android.gms.vision.barcode.Barcode.ALL_FORMATS
+import com.google.mlkit.vision.barcode.common.Barcode.FORMAT_ALL_FORMATS
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-class BarcodeScannerViewHandler(private val scope: ActivityScope) : ViewHandler<BarcodeScannerView>(scope) {
+class BarcodeScannerViewHandler(private val scope: ActivityScope) :
+    ViewHandler<BarcodeScannerView>(scope) {
 
-  override val type = "com.eclipsesource.barcodescanner.BarcodeScannerView"
+    override val type = "com.eclipsesource.barcodescanner.BarcodeScannerView"
 
-  override val properties: List<Property<*, *>> by lazy {
-    super.properties + listOf<Property<BarcodeScannerView, *>>(
-        StringProperty("scaleMode", { scaleMode = ScaleMode.find(it) ?: ScaleMode.FIT })
-    )
-  }
-
-  override fun create(id: String, properties: V8Object): BarcodeScannerView {
-    return BarcodeScannerView(scope).apply {
-      camera = if (properties.getStringOrNull("camera") == "front") CAMERA_FACING_FRONT else CAMERA_FACING_BACK
-      scope.events.addActivityStateListener(this)
+    override val properties: List<Property<*, *>> by lazy {
+        super.properties + listOf<Property<BarcodeScannerView, *>>(
+            StringProperty("scaleMode", { scaleMode = ScaleMode.find(it) ?: ScaleMode.FIT })
+        )
     }
-  }
 
-  override fun call(scannerView: BarcodeScannerView, method: String, properties: V8Object): Any? {
-    return when (method) {
-      "start" -> scannerView.start(getFormats(properties))
-      "stop" -> scannerView.stop()
-      else -> super.call(scannerView, method, properties)
+    override fun create(id: String, properties: V8Object): BarcodeScannerView {
+        val isFrontFacing = properties.getStringOrNull("camera") == "front"
+        val camera = if (isFrontFacing) DEFAULT_FRONT_CAMERA else DEFAULT_BACK_CAMERA
+        return BarcodeScannerView(scope, camera)
     }
-  }
 
-  private fun getFormats(properties: V8Object): Int {
-    val formatIds = properties.getArrayOrNull("formats")?.asSequence<String>()?.map {
-      BARCODE_NAMES.entries.firstOrNull { entry -> entry.value == it }?.key ?: ALL_FORMATS
-    } ?: sequenceOf(ALL_FORMATS)
-    return formatIds.fold(ALL_FORMATS, Int::or)
-  }
+    override fun call(scannerView: BarcodeScannerView, method: String, properties: V8Object): Any? {
+        return when (method) {
+            "start" -> scannerView.start(getFormats(properties))
+            "stop" -> scannerView.stop()
+            else -> super.call(scannerView, method, properties)
+        }
+    }
 
-  override fun destroy(scannerView: BarcodeScannerView) {
-    scannerView.stop()
-    scope.events.removeActivityStateListener(scannerView)
-    super.destroy(scannerView)
-  }
+    private fun getFormats(properties: V8Object): Int {
+        val formatIds = properties.getArrayOrNull("formats")?.asSequence<String>()?.map { format ->
+            BARCODE_NAMES.entries.firstOrNull { it.value == format }?.key ?: FORMAT_ALL_FORMATS
+        } ?: sequenceOf(FORMAT_ALL_FORMATS)
+        return formatIds.fold(FORMAT_ALL_FORMATS, Int::or)
+    }
+
+    override fun destroy(scannerView: BarcodeScannerView) {
+        scannerView.stop()
+        super.destroy(scannerView)
+    }
 
 }
